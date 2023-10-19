2f32beec66321bb230fa404f4801dbb95452788e
Дмитрий Шиманский <yaflay@vk.com>
2023-10-19 18:59:59 +0300

    Rework controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Xml.Schema;
+
+namespace yaflay.ru.Новая_папка
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+
+        private async Task<string> getUrlFromGit(string baseUrl)
+        {
+            HttpClient client = new();
+            string Base64BearerToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(""));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Base64BearerToken);
+            HttpResponseMessage getter =  await client.GetAsync("https://raw.githubusercontent.com/YaFlay/yaflay.ru/master/redirect_uris.json?token=GHSAT0AAAAAAB54DYE4TFGPWCCPBHAGGZR4ZJRKIVA");
+            JsonNode allFile = JsonNode.Parse(await getter.Content.ReadAsStringAsync());
+            return (string?)allFile[baseUrl];
+        }
+        // GET: HomeController/Details/5
+        [HttpGet]
+        public async Task<IActionResult> fromGitHub()
+        {
+            string? url = await getUrlFromGit(HttpContext.Request.Path.Value);
+
+            return Redirect(url != null ? url : "yaflay.ru");
+        }
+
+        // GET: HomeController/Create
+    }
+}
