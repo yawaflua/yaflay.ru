abdf1da3d51c434b7b8ab387dccc0e6f54812626
Дмитрий Шиманский <yaflay@vk.com>
2023-10-20 18:19:44 +0300

    Fix bug

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Xml.Schema;
 
@@ -21,7 +22,10 @@ namespace yaflay.ru.Новая_папка
                 HttpClient client = new();
                 HttpResponseMessage getter = await client.GetAsync("https://raw.githubusercontent.com/YaFlay/yaflay.ru/master/redirect_uris.json");
                 await Console.Out.WriteLineAsync(await getter.Content.ReadAsStringAsync());
-                JsonNode allFile = JsonNode.Parse(await getter.Content.ReadAsStringAsync());
+                JsonNodeOptions jsonNodeOptions = new ();
+                JsonDocumentOptions jsonDocumentOptions = new();
+                jsonDocumentOptions.AllowTrailingCommas = true;
+                JsonNode allFile = JsonNode.Parse(await getter.Content.ReadAsStringAsync(), jsonNodeOptions, jsonDocumentOptions);
                 return (string?)allFile[baseUrl];
             }
             catch (Exception except)
