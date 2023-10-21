11cc55256f25ea7fd89c6e56b3e3687397ac7979
Дмитрий Шиманский <yaflay@vk.com>
2023-10-22 01:38:12 +0300

    make oembed

--- Startup.cs
+++ Startup.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.Extensions.Options;
-
+using HeyRed.OEmbed;
 namespace yaflay.ru
 {
     public class Startup
     {
         private readonly IConfiguration configuration;
+        private readonly OEmbedOptions options;
         public Startup()
         {
             configuration = new ConfigurationBuilder()
                 .AddEnvironmentVariables(prefix: "m.")
                 .AddJsonFile("appsettings.json", optional: true)
                 .Build();
+            options = new OEmbedOptions()
+            {
+                EnableCache = true
+            };
         }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddRouting();
             services.AddRazorPages();
+            services.AddOEmbed(options =>
+            {
+                options.EnableCache = true; // true by default
+                options.EnsureNotNull();
+            });
             //services.AddDirectoryBrowser();
-            
-            
-            
+
+
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
