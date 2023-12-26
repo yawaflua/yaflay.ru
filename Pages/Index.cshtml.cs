e2800cfc2ea1d4b2966c10f9f154699c7d7274ac
Дмитрий Шиманский <yaflay@vk.com>
2023-12-26 13:23:26 +0300

    some code review

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,17 +6,17 @@ namespace yaflay.ru.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public string? uri { get; set; } = null;
+        //public string? uri { get; set; } = null;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
             
         }
 
-        public void OnGet(string? uri)
+        public void OnGet(/**string? uri**/)
         {
             Page();
-            this.uri = uri ?? null;
+            //this.uri = uri ?? null;
         }
 
     }
